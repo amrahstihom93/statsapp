417bb7723bb27097762cfe867f16ebe28cd6b2ae
amrahstihom93 <amrahstihom93@gmail.com>
2019-03-30 12:03:19 +0530

    .
    .

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -2,7 +2,6 @@ $("#navbarDropdown").click(function(e) {
   e.preventDefault();
   var x = document.getElementById("sidebar");
   var y = document.getElementById("sidebar2");
-
   $("#sidebar").toggleClass("active");
   var clsname=document.getElementById("cntnt");
   var dd = clsname.getAttribute("class");
@@ -12,14 +11,10 @@ $("#navbarDropdown").click(function(e) {
   if(dd == "content full-cntnt"){
       clsname.classList.remove("full-cntnt");
       clsname.classList.add("full-cntnt1");
-      x.style.display = "none";
-      y.style.display = "inline-block";
   }
   else{
       clsname.classList.remove("full-cntnt1");
       clsname.classList.add("full-cntnt");
-      x.style.display = "inline-block";
-      y.style.display = "none";
   }
 });
 
@@ -28,4 +23,21 @@ $('.dropdown-submenu a.test').on("click", function(e){
     $(this).next('div').toggle();
     e.stopPropagation();
     e.preventDefault();
+});
+
+
+
+$(document).ready(function() {
+    $(".y").mouseover(function () {
+        var x = document.getElementById("sidebar");
+        var y = document.getElementById("sidebar2");
+            x.style.display = "inline-block";
+            y.style.display = "none"
+    }),
+    $(".x").mouseout(function () {
+        var x = document.getElementById("sidebar");
+        var y = document.getElementById("sidebar2");
+            x.style.display = "none";
+            y.style.display = "inline-block"
+    })
 });
